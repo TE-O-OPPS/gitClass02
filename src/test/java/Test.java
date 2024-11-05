97a2999999e392013ab78975d1a1d58f41d94284
TE-O-OPP <tekia2001@gmail.com>
2024-11-04 20:31:06 -0500

    my first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is a new project ");
+        System.out.println("This is a version 1");
+    }
+}
