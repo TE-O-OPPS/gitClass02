454d15ac8f2ae3ed74c8b53b33f812730da268a6
TE-O-OPP <tekia2001@gmail.com>
2024-11-05 12:12:34 -0500

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is a new project ");
+        
         System.out.println("This is a version 1");
     }
 }
+//I'm done with version four.
